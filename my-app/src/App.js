5402952b58914eaa96ed77f55c71a11a80227158
Nikolay Gorokhov <nikolay.gorokhov@ya.ru>
2023-12-06 13:57:47 +0300

    edit App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+	const data = new Date();
+	const nowYear = data.getFullYear();
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit',
+				createElement('code', null, 'src/App.js'),
+				'and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('span', null, nowYear),
+		),
 	);
 }
 
