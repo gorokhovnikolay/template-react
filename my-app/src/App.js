2220df28bd2c9b2b2158c1b8abeb46f994315425
Nikolay Gorokhov <nikolay.gorokhov@ya.ru>
2023-12-06 16:44:13 +0300

    createElement

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 // App.js имеет декларативный стиль. Императивного стиля не нашел
 
 export const App = () => {
 	const data = new Date();
 	const nowYear = data.getFullYear();
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<span>{nowYear}</span>
-			</header>
-		</div>
+
+	return createElement(
+		'div',
+		{ className: 'App' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					lassName: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('span', null, nowYear),
+		),
 	);
 };
